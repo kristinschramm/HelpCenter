3d32396636742dad2807b060af27b483d6305856
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-11 14:20:28 -0500

    complete Models.Unit

---
+++ HelpCenter/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpCenter.Models
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+
+        public string Number { get; set; }
+
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+    }
+}
