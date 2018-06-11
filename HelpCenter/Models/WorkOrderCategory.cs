01a11ced6fb91a3f03759cf97efd45fa13e408cf
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-11 14:09:20 -0500

    complete Models.WorkOrderCategory

---
+++ HelpCenter/Models/WorkOrderCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpCenter.Models
+{
+    public class WorkOrderCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
