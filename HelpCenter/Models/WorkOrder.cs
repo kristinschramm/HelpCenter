d62c6524f86dd0083f0928555120fdcd3a0622ed
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-11 14:07:40 -0500

    completed Models.WorkOrder

---
+++ HelpCenter/Models/WorkOrder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpCenter.Models
+{
+    public class WorkOrder
+    {
+        public int Id { get; set; }
+
+        public string Subject { get; set; }
+
+        public int? CategoryId { get; set; }
+        public WorkOrderCategory Category { get; set; }
+
+        public int StatusId { get; set; }
+        public WorkOrderStatus Status { get; set; }
+
+        public DateTime StatusDateTime { get; set; }
+
+        public string RequestorId { get; set; }
+        public User Requestor { get; set; }
+
+        public int? LocationId { get; set; }
+        public Location Location { get; set; }
+
+        public int? UnitId { get; set; }
+        public Unit Unit { get; set; }
+
+        public string AssignedUserId { get; set; }
+        public User AssignedUser { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        public DateTime ModifiedDateTime { get; set; }
+
+        public DateTime? ExpectedCompletionDateTime { get; set; }
+
+        public double? AccountCharge { get; set; }
+
+
+    }
+}
