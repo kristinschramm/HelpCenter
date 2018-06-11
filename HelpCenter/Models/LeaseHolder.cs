6669b079ff4f83d37b96043b21703fd618478bee
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-11 14:29:46 -0500

    complete Models.User & Models.LeaseHolder

---
+++ HelpCenter/Models/LeaseHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpCenter.Models
+{
+    public class LeaseHolder : User
+    {
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+
+        public int UnitId { get; set; }
+        public Unit Unit { get; set; }
+    }
+}
