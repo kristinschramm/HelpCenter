6bf8e8b88c4f48c8fb820ab4d8206019cf1489c3
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-11 14:10:40 -0500

    complete Models.WorkOrderStatus

---
+++ HelpCenter/Models/WorkOrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpCenter.Models
+{
+    public class WorkOrderStatus
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOpen { get; set; }
+    }
+}
