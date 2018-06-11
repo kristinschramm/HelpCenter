cb0cf7f80170b9e104ba0fc10d45ebe2c0130432
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-11 14:40:15 -0500

    complete Models.Transaction

---
+++ HelpCenter/Models/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpCenter.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public DateTime TransactionDateTime { get; set; }
+
+        public string Description { get; set; }
+
+        public int? WorkOrderId { get; set; }
+        public WorkOrder WorkOrder { get; set; }
+
+        public string LeaseHolderId { get; set; }
+        public LeaseHolder LeaseHolder { get; set; }
+
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+
+        public int UnitId { get; set; }
+        public Unit Unit { get; set; }
+
+        public double Amount { get; set; }
+
+
+    }
+}
