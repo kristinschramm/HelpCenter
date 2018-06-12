c699f02dddb39513a83703ef5dd65b8e0e80ddf5
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-12 08:28:48 -0500

    completed Controllers.WorkOrder.Index()

---
+++ HelpCenter/Controllers/WorkOrderController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelpCenter.Models;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+
+namespace HelpCenter.Controllers
+{
+    [Authorize]
+    public class WorkOrderController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public WorkOrderController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        // GET: WorkOrder
+        public ActionResult Index()
+        {
+            IEnumerable<WorkOrder> workOrders;
+
+            if (User.IsInRole(RoleName.LeaseHolder))
+            {
+                var userId = User.Identity.GetUserId();
+
+                workOrders = _context.WorkOrders
+                    .Include(w => w.AssignedUser)
+                    .Include(w => w.Category)
+                    .Include(w => w.Location)
+                    .Include(w => w.Unit)
+                    .Include(w => w.Requestor)
+                    .Include(w => w.Status)
+                    .Where(w => w.RequestorId == userId)
+                    .ToList();
+            }
+            else
+            {
+                workOrders = _context.WorkOrders
+                    .Include(w => w.AssignedUser)
+                    .Include(w => w.Category)
+                    .Include(w => w.Location)
+                    .Include(w => w.Unit)
+                    .Include(w => w.Requestor)
+                    .Include(w => w.Status)
+                    .ToList();
+            }
+
+            return View(workOrders);
+        }
+    }
+}
