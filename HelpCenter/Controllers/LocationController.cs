5372c89f0e12ac228a0c4c470343d2671fdb3a66
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-12 13:11:20 -0500

    created Controllers.LocationController

---
+++ HelpCenter/Controllers/LocationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelpCenter.Models;
+
+namespace HelpCenter.Controllers
+{
+    public class LocationController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public LocationController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Location
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
