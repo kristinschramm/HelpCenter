3e98bc78ae265dc720e455b40e074bbee2610908
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-12 14:31:24 -0500

    completed Controllers.LocationController.Edit()

--- HelpCenter/Controllers/LocationController.cs
+++ HelpCenter/Controllers/LocationController.cs
@@ -23,7 +23,38 @@ namespace HelpCenter.Controllers
             var locations = _context.Locations
                 .OrderBy(l => l.Name)
                 .ToList();
-            return View();
+
+            return View(locations);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var location = _context.Locations.Single(l => l.Id == id);
+
+            return View(location);
+        }
+
+        public ActionResult Edit (int id)
+        {
+            var location = _context.Locations.Where(l => l.Id == id);
+
+            return View(location);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Location location)
+        {
+            var locationInDb = _context.Locations.Single(l => l.Id == id);
+
+            locationInDb.Name = location.Name;
+            locationInDb.Address = location.Address;
+            locationInDb.City = location.City;
+            locationInDb.State = location.State;
+            locationInDb.Zip = location.Zip;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
