677d3130ba41c1ed0a643945cc3cb585c501ad09
ckloskey <ckloskey@gmail.com>
2018-06-13 14:35:16 -0500

    re-committing outboudmessage class in server

---
+++ Server/GmailOutboundMessage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Net.Mail;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace Server
+{
+    public class GmailOutboundMessage
+    {
+
+        public void SendIt(string requestorEmailAddress, string subject, string body)
+        {
+
+            var msg = new AE.Net.Mail.MailMessage
+            {
+                Subject = subject,
+                Body = body,
+                From = new MailAddress("devseleniumhelpdesk@gmail.com")
+            };
+            msg.To.Add(new MailAddress(requestorEmailAddress));
+            msg.ReplyTo.Add(msg.From); // Bounces without this!!
+            var msgStr = new StringWriter();
+            msg.Save(msgStr);
+
+
+            // Context is a separate bit of code that provides OAuth context;
+            // your construction of GmailService will be different from mine.
+            var gmail = new GmailService(new BaseClientService.Initializer());
+            var result = gmail.Users.Messages.Send(new Message
+            {
+                Raw = Base64UrlEncode(msgStr.ToString())
+            }, "me").Execute();
+        }
+
+        private static string Base64UrlEncode(string input)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            // Special "url-safe" base64 encode.
+            return Convert.ToBase64String(inputBytes)
+              .Replace('+', '-')
+              .Replace('/', '_')
+              .Replace("=", "");
+        }
+
+    }
+}
