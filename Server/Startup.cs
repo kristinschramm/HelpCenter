dbac2b7704298671c3b36504a7a0e16e9fe7f3a1
Jacob Hansen <hansenjacobs@gmail.com>
2018-06-13 10:16:51 -0500

    add server project and created db connections and GMail retrival

---
+++ Server/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Server;
+
+[assembly: OwinStartupAttribute(typeof(Server.Startup))]
+namespace Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // ConfigureAuth(app);
+            GmailRetrieval.GmailStart();
+        }
+    }
+}
