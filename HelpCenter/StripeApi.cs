0464c9d940c9e8a681552c2a62e7236b57709f49
ckloskey <ckloskey@gmail.com>
2018-06-14 14:51:22 -0500

    added stripe api connection and installed stripe.net package

---
+++ HelpCenter/StripeApi.cs
@@ -0,0 +1,43 @@
+﻿//using ServiceStack.Stripe;
+using Stripe;
+using System.Web.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HelpCenter.Ignored;
+
+namespace HelpCenter
+{
+    public class StripeApi
+    {
+        public static void LaunchStripe()
+        {
+            StripeConfiguration.SetApiKey(APIKeys.Stripe);
+        }
+
+        public static void NewTenant()
+        {
+            var customerOptions = new StripeCustomerCreateOptions()
+            {
+
+            };
+
+            var customerService = new StripeCustomerService();
+            StripeCustomer customer = customerService.Create(customerOptions);
+        }
+
+        public static void ChargeTenant()
+        {
+            var options = new StripeChargeCreateOptions
+            {
+                Amount = 999,
+                Currency = "usd",
+                SourceTokenOrExistingSourceId = "tok_visa",
+                ReceiptEmail = "jenny.rosen@example.com",
+            };
+            var service = new StripeChargeService();
+            // StripeCharge charge = service.Create(chargeOptions);
+        }
+    }
+}
